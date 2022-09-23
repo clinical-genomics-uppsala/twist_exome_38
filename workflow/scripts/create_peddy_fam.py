4995ff7635f942bb3bf1722afbcfb7b1c978985c
Padraic Corcoran <Padraic Corcoran>
2022-09-23 16:17:02 +0200

    Changes how the trio information is extracted as the location can vary in the description column

--- workflow/scripts/create_peddy_fam.py
+++ workflow/scripts/create_peddy_fam.py
@@ -4,7 +4,7 @@
 import pandas as pd
 import sys
 
-def get_sex(sex):
+def get_ped_sex(sex):
 
     if sex == 'M':
         sex_code = '1'
@@ -15,18 +15,49 @@ def get_sex(sex):
 
     return sex_code
 
+def extract_sex(col):
+    col_list = col.split('_')
+    sex = 'NA'
+    for _ in col_list:
+        if _ in ['M', 'K']:
+            sex = _
+
+    return sex
+
+
+def extract_trio_id(col):
+    col_list = col.split('_')
+    trio_id = 'NA'
+    for _ in col_list:
+        if _.startswith('Trio'):
+            trio_id =  _
+
+    return trio_id
+
+
+# read in the sample sheet
+# df = pd.read_csv('config/SampleSheet.csv', header=13,
+#   dtype=str).set_index("Sample_ID", drop=False)
+
 df = pd.read_csv(snakemake.input[0], header=13,
   dtype=str).set_index("Sample_ID", drop=False)
 
-df[['Code', 'Sex', 'NA', '123', 'Trio']] = df.Description.str.split('_', expand=True)
 
-trio_ids = df.Trio.tolist() # checking for trio ids in the Trio column
-if len(set(trio_ids)) == 1 and trio_ids[0] == 'NA' :
-    print('No Trios detected in Sample Sheet')
-    df['Trio_Status'] = ['NA']*df.shape[0]
-    df['FID'] = ['NA']*df.shape[0]
-else:
-    df[['FID', 'Trio_Status']] = df.Trio.str.split('-', expand=True)
+df["Sex"] = df.Description.apply(extract_sex)
+df["Trio"] = df.Description.apply(extract_trio_id)
+
+def add_trio_status(df):
+    trio_ids = df.Trio.tolist() # checking for trio ids in the Trio column
+    if len(set(trio_ids)) == 1 and trio_ids[0] == 'NA' :
+        print('No Trios detected in Sample Sheet')
+        df['Trio_Status'] = ['NA']*df.shape[0]
+        df['FID'] = ['NA']*df.shape[0]
+    else:
+        df[['FID', 'Trio_Status']] = df.Trio.str.split('-', expand=True)
+
+    return df
+
+df = add_trio_status(df)
 
 fam_df  = df[['Sample_ID', 'Sex', 'FID', 'Trio_Status']]
 
@@ -35,7 +66,7 @@ father_df = fam_df[(df.Trio_Status == 'Foralder') & (df.Sex == 'M')]
 mother_df = fam_df[(df.Trio_Status == 'Foralder') & (df.Sex == 'K')]
 
 with open(snakemake.output[0], 'w') as pedfile:
-    phenotype = '0'
+    phenotype = '-9'
 
     for sample in fam_df.itertuples():
         sample_id = sample.Sample_ID + '_N' # add the '_N'
@@ -51,6 +82,6 @@ with open(snakemake.output[0], 'w') as pedfile:
             paternal_id = '0'
             maternal_id = '0'
 
-        sex = get_sex(sample.Sex)
+        sex = get_ped_sex(sample.Sex)
         print(family_id, sample_id ,paternal_id, maternal_id,
          sex, phenotype, sep='\t', file=pedfile )
